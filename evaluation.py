7b594b70fb3f07c95efad4377c08f1beb4098725
Tyler <141290273+trobe124@users.noreply.github.com>
2023-12-09 13:32:53 -0500

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -1,137 +1,114 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Anna Kuhn
+# Copyright (C) 2021 University of Zurich
+
 '''
 Created on 15.12.2014
 
 @author: Peter U. Diehl
+https://github.com/zxzhijia/Brian2STDPMNIST
 '''
 
 import numpy as np
-import matplotlib
-import matplotlib.cm as cmap
-import time
-import os.path
-import scipy 
-import cloudpickle as pickle
-from struct import unpack
-import brian2 as b
-from brian2 import *
 
-#------------------------------------------------------------------------------ 
-# functions
-#------------------------------------------------------------------------------     
-def get_labeled_data(picklename, bTrain = True):
-    """ Read input-vector (image) and target class (label, 0-9) 
-    and return as list of tuples
-    """
-    if os.path.isfile('%s.pickle' % picklename):
-        data = pickle.load(open('%s.pickle' % picklename, 'rb'))
-    else:
-        # open gzip images in read binary mode
-        if bTrain:
-            images = open(MNIST_data_path + 'train-images-idx3-ubyte', 'rb')
-            labels = open(MNIST_data_path + 'train-labels-idx1-ubyte', 'rb')
-        else:
-            images = open(MNIST_data_path + 't10k-images-idx3-ubyte', 'rb')
-            labels = open(MNIST_data_path + 't10k-labels-idx1-ubyte', 'rb')
-        # get metadata for images
-        images.read(4) #skip magic number
-        number_of_images = unpack('>I', images.read(4))[0]
-        rows = unpack('>I', images.read(4))[0]
-        cols = unpack('>I', images.read(4))[0]
-        # get metadata for labels
-        labels.read(4) #skip magic number
-        N = unpack('>I', labels.read(4))[0]
-        
-        if number_of_images != N:
-            raise Exception('Number of labels did not match number of images!')
-        # get data
-        x = np.zeros((N, rows, cols), dtype=np.uint8) # initialize numpy array
-        y = np.zeros((N,1), dtype=np.uint8) # initialize numpy array
-        for i in range(N):
-            if i % 1000 == 0:
-                print("i: %i" % i)
-            x[i] = [[unpack('>B', images.read(1))[0] for unused_col in range(cols)] for unused_row in range(rows)]
-            y[i] = unpack('>B', labels.read(1))[0]
-        
-        data = {'x': x, 'y': y, 'rows': rows, 'cols': cols}
-        pickle.dump(data, open("%s.pickle" % picklename, "wb"))
-    return data
+from brian2 import *
+from brian2tools import *
+
+#------------------------------------------------------------------------------
+# Functions
+#------------------------------------------------------------------------------
+
 
 def get_recognized_number_ranking(assignments, spike_rates):
-    summed_rates = [0] * 10
-    num_assignments = [0] * 10
+    """
+    Funtion to assign a related class to each neuron
+    """
+    summed_rates = [0]*10
+    num_assignments = [0]*10
     for i in range(10):
         num_assignments[i] = len(np.where(assignments == i)[0])
         if num_assignments[i] > 0:
-            summed_rates[i] = np.sum(spike_rates[assignments == i]) / num_assignments[i]
+            summed_rates[i] = (np.sum(spike_rates[assignments == i]) 
+                / num_assignments[i])
     return np.argsort(summed_rates)[::-1]
 
+
 def get_new_assignments(result_monitor, input_numbers):
+    """
+    Funtion to predict a class to input images
+    """
     print(result_monitor.shape)
-    assignments = np.ones(n_e) * -1 # initialize them as not assigned
+    assignments = np.ones(n_e) * -1 # Init them as not assigned
     input_nums = np.asarray(input_numbers)
     maximum_rate = [0] * n_e    
     for j in range(10):
         num_inputs = len(np.where(input_nums == j)[0])
         if num_inputs > 0:
-            rate = np.sum(result_monitor[input_nums == j], axis = 0) / num_inputs
+            rate = (np.sum(result_monitor[input_nums == j], axis = 0) 
+                / num_inputs)
         for i in range(n_e):
             if rate[i] > maximum_rate[i]:
                 maximum_rate[i] = rate[i]
                 assignments[i] = j 
     return assignments
 
-MNIST_data_path = '/Users/TyTheWiseGuy/brian2/MNIST/'
+
+MNIST_data_path = '/Users/TyTheWiseGuy/brian2/MNIST'
 data_path = '/Users/TyTheWiseGuy/brian2/Brian2STDPMNIST/activity/'
-training_ending = '10000'
-testing_ending = '10000'
+training_ending = '400'
+testing_ending = '400'
 start_time_training = 0
 end_time_training = int(training_ending)
 start_time_testing = 0
 end_time_testing = int(testing_ending)
 
-n_e = 400 # Number of excitatory neurons
-n_input = 784 # Number of input neurons
+n_e = 400 
+n_input = 784
 ending = ''
 
-print('Load MNIST\n')
-training = get_labeled_data(MNIST_data_path + 'training')
-testing = get_labeled_data(MNIST_data_path + 'testing', bTrain = False)
 
-print('Load results')
-training_result_monitor = np.load(data_path + 'resultPopVecs' + training_ending + ending + '.npy')
-training_input_numbers = np.load(data_path + 'inputNumbers' + training_ending + '.npy')
-testing_result_monitor = np.load(data_path + 'resultPopVecs' + testing_ending + '.npy')
-testing_input_numbers = np.load(data_path + 'inputNumbers' + testing_ending + '.npy')
+print('load results')
+training_result_monitor = (np.load(data_path + 'resultPopVecs' 
+    + training_ending + ending + '.npy'))
+training_input_numbers = (np.load(data_path + 'inputNumbers' 
+    + training_ending + '.npy'))
+testing_result_monitor = (np.load(data_path + 'resultPopVecs' 
+    + testing_ending + '.npy'))
+testing_input_numbers = (np.load(data_path + 'inputNumbers' 
+    + testing_ending + '.npy'))
 print(training_result_monitor.shape)
-print('')
 
-print('Get assignments\n')
+print('get assignments')
 test_results = np.zeros((10, end_time_testing-start_time_testing))
 test_results_max = np.zeros((10, end_time_testing-start_time_testing))
 test_results_top = np.zeros((10, end_time_testing-start_time_testing))
 test_results_fixed = np.zeros((10, end_time_testing-start_time_testing))
-assignments = get_new_assignments(training_result_monitor[start_time_training:end_time_training], training_input_numbers[start_time_training:end_time_training])
+assignments = get_new_assignments(
+    training_result_monitor[start_time_training:end_time_training], 
+    training_input_numbers[start_time_training:end_time_training])
 print(assignments)
-
 counter = 0 
-num_tests = end_time_testing / 10000
-my_list = [0]
-my_array = np.array(my_list)
-sum_accuracy = my_array * num_tests
-while (counter < num_tests):
-    end_time = min(end_time_testing, 10000*(counter+1))
-    start_time = 10000*counter
+num_tests = int(end_time_testing / end_time_testing)
+sum_accurracy = [0] * num_tests
+while(counter < num_tests):
+    end_time = min(end_time_testing, end_time_testing*(counter+1))
+    start_time = end_time_testing*counter
     test_results = np.zeros((10, end_time-start_time))
-    print('\nCalculate accuracy of performance:')
+    print('calculate accuracy for sum')
     for i in range(end_time - start_time):
-        test_results[:,i] = get_recognized_number_ranking(assignments, testing_result_monitor[i+start_time,:])
-    
-    difference = test_results[0,:] - testing_input_numbers[start_time:end_time]
+        test_results[:,i] = get_recognized_number_ranking(assignments, 
+        testing_result_monitor[i+start_time,:])
+    difference = (test_results[0,:] 
+        - testing_input_numbers[start_time:end_time])
     correct = len(np.where(difference == 0)[0])
     incorrect = np.where(difference != 0)[0]
-    sum_accuracy[counter] = correct/float(end_time-start_time) * 100
-    print('The accuracy is ', sum_accuracy[counter], '%. The number of incorrect readings is ', len(incorrect), ' out of 10000.')
+    sum_accurracy[counter] = correct/float(end_time-start_time) * 100
+    print('Sum response - accuracy: ', sum_accurracy[counter], 
+        ' num incorrect: ', len(incorrect))
     counter += 1
-#print('Sum response - accuracy --> mean: ', np.mean(sum_accuracy),  '--> standard deviation: ', np.std(sum_accuracy))
+print('Sum response - accuracy --> mean: ', np.mean(sum_accurracy),  
+    '--> standard deviation: ', np.std(sum_accurracy))
 
-b.show()
+
+show()
+
--- random_conn_generator.py
+++ random_conn_generator.py
@@ -1,86 +1,96 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Anna Kuhn
+# Copyright (C) 2021 University of Zurich
 '''
 Created on 15.12.2014
-
 @author: Peter U. Diehl
+https://github.com/zxzhijia/Brian2STDPMNIST
 '''
-
-import scipy.ndimage as sp
 import numpy as np
-import pylab
-
-
 def randomDelay(minDelay, maxDelay):
+    """
+    Function to set an optional random delay anywhere
+    """
     return np.random.rand()*(maxDelay-minDelay) + minDelay
-        
-        
 def computePopVector(popArray):
+    """
+    Function to compute Vector Position
+    """
     size = len(popArray)
-    complex_unit_roots = np.array([np.exp(1j*(2*np.pi/size)*cur_pos) for cur_pos in range(size)])
-    cur_pos = (np.angle(np.sum(popArray * complex_unit_roots)) % (2*np.pi)) / (2*np.pi)
+    complex_unit_roots = (np.array([np.exp(1j * (2*np.pi / size) * cur_pos) \
+        for cur_pos in range(size)]))
+    cur_pos = (np.angle(np.sum(popArray * complex_unit_roots)) % (2*np.pi)) \
+        / (2*np.pi)
     return cur_pos
-
-        
 def sparsenMatrix(baseMatrix, pConn):
+    """
+    Function to delete some elements in the matrix with lasting connections of
+    probability pConn
+    """
     weightMatrix = np.zeros(baseMatrix.shape)
     numWeights = 0
     numTargetWeights = baseMatrix.shape[0] * baseMatrix.shape[1] * pConn
     weightList = [0]*int(numTargetWeights)
     while numWeights < numTargetWeights:
-        idx = (np.int32(np.random.rand()*baseMatrix.shape[0]), np.int32(np.random.rand()*baseMatrix.shape[1]))
+        idx = (np.int32(np.random.rand()*baseMatrix.shape[0]), 
+            np.int32(np.random.rand()*baseMatrix.shape[1]))
         if not (weightMatrix[idx]):
             weightMatrix[idx] = baseMatrix[idx]
             weightList[numWeights] = (idx[0], idx[1], baseMatrix[idx])
             numWeights += 1
     return weightMatrix, weightList
-        
-    
 def create_weights():
-    
+    """
+    Function to create weights for all connections and their weightings
+    """
     nInput = 784
     nE = 400
     nI = nE 
-    dataPath = '/Users/TyTheWiseGuy/brian2/Brian2STDPMNIST/weights/random'
+    dataPath = '/Users/TyTheWiseGuy/brian2/Brian2STDPMNIST/random/'
     weight = {}
-    weight['ee_input'] = 0.3 
-    weight['ei_input'] = 0.2 
+    weight['ee_input'] = 0.3
+    weight['ei_input'] = 0.2
     weight['ee'] = 0.1
     weight['ei'] = 10.4
     weight['ie'] = 17.0
     weight['ii'] = 0.4
     pConn = {}
-    pConn['ee_input'] = 1.0 
-    pConn['ei_input'] = 0.1 
+    pConn['ee_input'] = 1.0
+    pConn['ei_input'] = 0.1
     pConn['ee'] = 1.0
     pConn['ei'] = 0.0025
     pConn['ie'] = 0.9
     pConn['ii'] = 0.1
     
     
-    print('Create random connection matrices')
+    print('create connection matrices from X->E')
     connNameList = ['XeAe']
     for name in connNameList:
         weightMatrix = np.random.random((nInput, nE)) + 0.01
         weightMatrix *= weight['ee_input']
         if pConn['ee_input'] < 1.0:
-            weightMatrix, weightList = sparsenMatrix(weightMatrix, pConn['ee_input'])
+            weightMatrix, weightList = sparsenMatrix(weightMatrix, 
+                pConn['ee_input'])
         else:
-            weightList = [(i, j, weightMatrix[i,j]) for j in range(nE) for i in range(nInput)]
+            weightList = [(i, j, weightMatrix[i,j]) for j in range(nE) 
+                for i in range(nInput)]
+        print('save connection matrix', name)
         np.save(dataPath+name, weightList)
     
     
     
-    print('create connection matrices from E->I which are purely random')
-    connNameList = ['XeAi']
+    print('create connection matrices from E->E which are purely random')
+    connNameList = ['AeAe']
     for name in connNameList:
-        weightMatrix = np.random.random((nInput, nI))
-        weightMatrix *= weight['ei_input']
-        weightMatrix, weightList = sparsenMatrix(weightMatrix, pConn['ei_input'])
+        weightMatrix = np.random.random((nE, nE))
+        weightMatrix *= weight['ee']
+        weightMatrix, weightList = sparsenMatrix(weightMatrix, pConn['ee'])
         print('save connection matrix', name)
         np.save(dataPath+name, weightList)
         
     
     
-    print('Create connection matrices from E->I which are purely random')
+    print('create connection matrices from E->I which are purely random')
     connNameList = ['AeAi']
     for name in connNameList:
         if nE == nI:
@@ -88,13 +98,14 @@ def create_weights():
         else:
             weightMatrix = np.random.random((nE, nI))
             weightMatrix *= weight['ei']
-            weightMatrix, weightList = sparsenMatrix(weightMatrix, pConn['ei'])
-        print('Save connection matrix', name)
+            weightMatrix, weightList = sparsenMatrix(weightMatrix, 
+                pConn['ei'])
+        print('save connection matrix', name)
         np.save(dataPath+name, weightList)
         
         
         
-    print ('Create connection matrices from I->E which are purely random')
+    print('create connection matrices from I->E which are purely random')
     connNameList = ['AiAe']
     for name in connNameList:
         if nE == nI:
@@ -102,25 +113,16 @@ def create_weights():
             weightMatrix *= weight['ie']
             for i in range(nI):
                 weightMatrix[i,i] = 0
-            weightList = [(i, j, weightMatrix[i,j]) for i in range(nI) for j in range(nE)]
+            weightList = [(i, j, weightMatrix[i,j]) for i in range(nI) 
+                for j in range(nE)]
         else:
             weightMatrix = np.random.random((nI, nE))
             weightMatrix *= weight['ie']
-            weightMatrix, weightList = sparsenMatrix(weightMatrix, pConn['ie'])
-        print('Save connection matrix', name)
+            weightMatrix, weightList = sparsenMatrix(weightMatrix, 
+                pConn['ie'])
+        print('save connection matrix', name)
         np.save(dataPath+name, weightList)
     
          
 if __name__ == "__main__":
     create_weights()
-    
-
-
-
-
-
-
-
-
-
-
